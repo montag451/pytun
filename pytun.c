montag451/pytun

commit fc5f4c95f78e5b1351a11969fa85ce5d07f75acc

Fixed DeprecationWarning
Fixed DeprecationWarning: PY_SSIZE_T_CLEAN will be required for '#' formats
52. https://docs.python.org/3/c-api/intro.html#include-files

pytun.c
@@ -1,3 +1,4 @@
+#define PY_SSIZE_T_CLEAN
 #include <Python.h>
 #include <string.h>
 #include <unistd.h>

commit c22d59b07bcf99c8e571f26333b9156c269adeaa

Fix a bug introduced by fc5f4c95f78e5b1351a11969fa85ce5d07f75acc
When PY_SSIZE_T_CLEAN is defined, PyArg_ParseTuple expects a
Py_ssize_t instead of an int to store the size of the data

pytun.c
@@ -684,7 +684,7 @@ static PyObject* pytun_tuntap_write(PyObject* self, PyObject* args)
 {
     pytun_tuntap_t* tuntap = (pytun_tuntap_t*)self;
     char* buf;
-    int len;
+    Py_ssize_t len;
     ssize_t written;
 
     if (!PyArg_ParseTuple(args, "s#:write", &buf, &len))